4b7c5932b22169ed18ae8791e635a2899d405f94
192saha <23m0565@iitb.ac.in>
2025-08-03 22:01:36 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -43,7 +43,7 @@ class HiringAssistant:
         tech_list = extract_tech_stack(self.collected_data["tech_stack"])
         prompt = get_question_prompt(tech_list)
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}]
         )
         return response.choices[0].message.content
